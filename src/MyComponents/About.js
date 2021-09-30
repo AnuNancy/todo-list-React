b92d384d2836fc83433d1ce4d15b2425ea4ae96a
AnuNancy <68043383+AnuNancy@users.noreply.github.com>
2021-09-30 09:50:57 +0530

    todo-list (React)
    Understood the basic concept of React while making todo-application.

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about MyComponents
+            <p>lorem34hjklkknbfgfhbjkklnjbnvgfgcxfxddxf
+                hghgvhjkmkkjhjhjggcgfcvbjkjkmmk klnkj
+            </p>
+        </div>
+    )
+}
